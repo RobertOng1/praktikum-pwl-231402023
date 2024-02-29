42e920b62d1e79248a5a5df5ead7d64b7a2f72de
RobertOng1 <robertradja68@gmail.com>
2024-02-29 09:42:27 +0700

    first commit

---
+++ resources/views/contoh.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+    <p class ="text-3x1 bg-red-500">HELLO</p>
+</body>
+</html>
