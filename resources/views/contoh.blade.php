acde4dcb086a4ee5a324a9bc86efb4cc8db3a1cb
RobertOng1 <robertradja68@gmail.com>
2024-03-05 16:30:41 +0700

    Second Commit

--- resources/views/contoh.blade.php
+++ resources/views/contoh.blade.php
@@ -3,10 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tes DaisyUI</title>
     @vite('resources/css/app.css')
-    <title>Document</title>
 </head>
 <body>
-    <p class ="text-3x1 bg-red-500">HELLO</p>
+    <p class="text-3xl bg-red-500">Hello !</p>
+    <div class="card lg:card-side bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
+  <div class="card-body">
+    <h2 class="card-title">New album is released!</h2>
+    <p>Click the button to listen on Spotiwhy app.</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Listen</button>
+    </div>
+  </div>
+</div>
 </body>
 </html>
