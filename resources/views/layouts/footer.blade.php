5f05b445370fa9ca2c35c8976002303457cc44d2
RobertOng1 <robertradja68@gmail.com>
2024-03-13 19:50:34 +0700

    Tugas 2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer footer-center p-5 bg-base-200 text-base-content rounded bottom-0 absolute">
+  
+      <p>Copyright © 2024 - All right reserved by RobertxDaisyUI</p>
+    
+  </footer>
